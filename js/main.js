6c9d2e1b62fb7fa681edbe01e150154636cbb0ff
Truongcot99 <63283415+Truongcot99@users.noreply.github.com>
2021-10-04 14:40:05 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+// const navToggle = document.querySelector('.nav-icon')
+// const navList = document.querySelector(".nav-link")
+
+// navToggle.addEventListener('click', function () {
+//     navList.classList.toggle('nav-active')
+// })
